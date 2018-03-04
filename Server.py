19f696a907e1e1ebdd8acf73959f014d8242511a
sanchayana2007 <sanchayana2007@gmail.com>
2018-03-04 20:47:32 +0530

    Working an TEsted ubuntu

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-import socket 
+import socket
 
 TCP_IP='127.0.0.1'
 TCP_PORT=5005
@@ -12,24 +12,25 @@ s.listen(1)
 print('Server Listning on Address ',TCP_IP,':',TCP_PORT)
 
 while True:
-	conn, addr  = s.accept()
-	print('Client received with address:',addr,':',TCP_PORT)
-	
-	try:
-		while True:
-			data= conn.recv(BUFFER_SIZE)
-			resp="received"
-			
-			if not data:
-				print('No data is received ')
-				break
-				
-	except (KeyboardInterrupt,socket.error,socket.herror,socket.timeout) as e :
-		print("Error",e.errno,e.strerror)
-		if e.errno=10053:
-			print("Client as connected ")
-			
+        conn, addr  = s.accept()
+        print('Client received with address:',addr,':',TCP_PORT)
+
+        try:
+                while True:
+                        data= conn.recv(BUFFER_SIZE)
+                        resp="received"
+                        conn.send(resp.encode())
+                        if not data.decode():
+                                print('No data is received ')
+                                break
+                        else:
+                                print("Data",data)
+        except (KeyboardInterrupt,socket.error,socket.herror,socket.timeout) as e :
+                print("Error",e.errno,e.strerror)
+                if e.errno==10053:
+
+                        print("Client as connected ")
+
 
 s.close()
-
 
